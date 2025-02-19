be147e247afad2af5c0ee6634bb6c7bd914c1cb7
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2025-02-20 01:12:26 +0200

    fix: display chart and buttons when not select token

--- templates/static/chart.js
+++ templates/static/chart.js
@@ -1,4 +1,7 @@
 let chart
+let selectedToken = null
+let previousPrice = null
+let currentPrice = null
 
 async function loadTokens() {
   const response = await fetch('/api/v1/tokens/')
@@ -19,8 +22,15 @@ async function fetchPrices(token) {
 }
 
 async function renderChart() {
+  if (!selectedToken) {
+    console.warn('No token selected')
+    return
+  }
   const data = await fetchPrices(selectedToken)
+  const currentPrice = data.prices[data.prices.length - 1]
   const ctx = document.getElementById('priceChart').getContext('2d')
+
+  updatePriceDisplay(currentPrice)
 
   const chartData = {
     labels: data.timestamps.map((ts) =>
@@ -47,19 +57,55 @@ async function renderChart() {
       data: chartData,
     })
   }
-}
 
+  document.getElementById('priceChart').style.display = 'block'
+  document.getElementById('price-container').style.display = 'block'
+  document.getElementById('buttons-container').style.display = 'flex'
+}
+function updatePriceDisplay(currentPrice) {
+  const priceDisplay = document.getElementById('current-price')
+
+  const formattedCurrentPrice = currentPrice.toFixed(2)
+
+  if (previousPrice !== null) {
+    const difference = currentPrice - previousPrice
+    const formattedDifference = difference.toFixed(2)
+
+    if (difference > 0) {
+      priceDisplay.innerHTML = `<span class="text-green-500">${formattedCurrentPrice} (+${formattedDifference})</span>`
+    } else {
+      priceDisplay.innerHTML = `<span class="text-red-500">${formattedCurrentPrice} (${formattedDifference})</span>`
+    }
+  } else {
+    priceDisplay.innerHTML = `${formattedCurrentPrice}`
+  }
+
+  previousPrice = currentPrice
+
+  const buyButton = document.getElementById('buy-button')
+  const sellButton = document.getElementById('sell-button')
+  buyButton.disabled = false
+  sellButton.disabled = false
+}
 function updateChart() {
   const tokenSelect = document.getElementById('token-select')
   selectedToken = tokenSelect.value
+    console.log(selectedToken)
   if (selectedToken) {
     renderChart()
+    document.getElementById('buy-button').disabled = false
+    document.getElementById('sell-button').disabled = false
+  } else {
+    document.getElementById('priceChart').style.display = 'none'
+    document.getElementById('price-container').style.display = 'none'
+    document.getElementById('buttons-container').style.display = 'none'
+    document.getElementById('buy-button').disabled = true
+    document.getElementById('sell-button').disabled = true
   }
 }
 
 function startAutoUpdate() {
   loadTokens()
-  renderChart()
   setInterval(() => {
     if (selectedToken) {
       renderChart()
