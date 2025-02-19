64e4c2574d6e9415a493361bc4bf6507a6950687
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2025-02-20 00:11:02 +0200

    feat: add base style for app

--- templates/static/chart.js
+++ templates/static/chart.js
@@ -1,47 +1,72 @@
-let chart;
+let chart
+
+async function loadTokens() {
+  const response = await fetch('/api/v1/tokens/')
+  const data = await response.json()
+  const tokenSelect = document.getElementById('token-select')
+
+  data.tokens.forEach((token) => {
+    const option = document.createElement('option')
+    option.value = token
+    option.textContent = token
+    tokenSelect.appendChild(option)
+  })
+}
+
+async function fetchPrices(token) {
+  const response = await fetch(`/api/v1/tokens/prices/${token}`)
+  return await response.json()
+}
 
 async function renderChart() {
-    const data = await fetch('/api/v1/tokens/prices/SOL').then((res) => res.json())
-    const ctx = document.getElementById('priceChart').getContext('2d');
-    const chartData = {
-        labels: data.timestamps.map(ts => new Date(ts * 1000).toLocaleTimeString()),
-        datasets: [
-            {
-                label: 'Token Price',
-                data: data.prices,
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1,
-                fill: false
-            }
-        ],
-    };
-    if (chart) {
-        chart.data.labels = chartData.labels;
-        chart.data.datasets[0].data = data.prices;
-        chart.update();
-    } else {
-        chart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: chartData.labels,
-                datasets: [
-                    {
-                        label: 'Token Price',
-                        data: data.prices,
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1,
-                    },
-                ],
-            },
-        });
-    }
+  const data = await fetchPrices(selectedToken)
+  const ctx = document.getElementById('priceChart').getContext('2d')
+
+  const chartData = {
+    labels: data.timestamps.map((ts) =>
+      new Date(ts * 1000).toLocaleTimeString(),
+    ),
+    datasets: [
+      {
+        label: `Token ${selectedToken} / USDC`,
+        data: data.prices,
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+        fill: false,
+      },
+    ],
+  }
+
+  if (chart) {
+    chart.data.labels = chartData.labels
+    chart.data.datasets[0].data = data.prices
+    chart.update()
+  } else {
+    chart = new Chart(ctx, {
+      type: 'line',
+      data: chartData,
+    })
+  }
+}
+
+function updateChart() {
+  const tokenSelect = document.getElementById('token-select')
+  selectedToken = tokenSelect.value
+  if (selectedToken) {
+    renderChart()
+  }
 }
 
 function startAutoUpdate() {
-    renderChart();
-    setInterval(renderChart, 5000);
+  loadTokens()
+  renderChart()
+  setInterval(() => {
+    if (selectedToken) {
+      renderChart()
+    }
+  }, 5000)
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    startAutoUpdate();
-});
+  startAutoUpdate()
+})
